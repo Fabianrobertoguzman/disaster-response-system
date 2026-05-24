5966a82b423d1dd2a9cef299572ff49848cace3a
Prabeg Shakya <shakyaprabeg@gmail.com>
2026-05-24 13:16:10 +1000

    Initial Commit

---
+++ src/main/java/com/coit20258/drs/Drs.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.coit20258.drs;
+
+/**
+ *
+ * @author nischala
+ */
+public class Drs {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/coit20258/drs/model/User.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.coit20258.drs.model;
+
+/**
+ *
+ * @author nischala
+ */
+public class User {
+    
+}
