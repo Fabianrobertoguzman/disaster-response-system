4b3560ce5e770618d5ea85bbc20836d0e9d3e251
Prabeg Shakya <shakyaprabeg@gmail.com>
2026-05-25 12:23:49 +1000

    Setup entry point to run javafx

--- src/main/java/com/coit20258/drs/Drs.java
+++ src/main/java/com/coit20258/drs/Drs.java
@@ -1,16 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.coit20258.drs;
 
-/**
- *
- * @author nischala
- */
-public class Drs {
+import java.util.logging.Logger;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import com.coit20258.drs.util.Database;
+import com.coit20258.drs.util.SceneManager;
+
+public class Drs extends Application {
+    
+    private static final Logger LOGGER = Logger.getLogger(Drs.class.getName());
+    
+    @Override
+    public void start(Stage primaryStage) {
+        LOGGER.info("DRS-Enhanced starting up...");
+ 
+//        Database.boot();
+        SceneManager.init(primaryStage, 1100, 720); 
+        SceneManager.switchTo("LoginView");
+ 
+        LOGGER.info("DRS-Enhanced ready.");
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        launch(args);
     }
 }
